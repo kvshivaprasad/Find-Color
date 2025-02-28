7bf7b3b6d90b7c63e11e322aaa0ebe5047b103c0
kvshivaprasad <kvshivaprasad816@gmail.com>
2025-02-28 23:01:59 +0530

    change the theme mode

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-// let colorcode = document.getElementById("colorhunt");
-// colorcode.addEventlisterner("onmouseover", () => {
-//   console.log("is over");
-// });
-// colorcode.onmouseover = () => {
-//   colorcode.classList.remove("colorhunt");
-//   console.log("hover");
-//   colorcode.innerText = "88d34";
-// };
-// colorcode.onmouseout = () => {
-//   colorcode.innerText = "";
-// };
 let maindiv = document.querySelector(".maindiv");
+let contener = document.getElementsByClassName("contener");
+let navoption = document.getElementsByClassName("navoption");
+let sidebar = document.querySelector("#sidebar");
+let aftermode = document.getElementsByClassName("aftermode");
+let seconddiv = document.getElementsByClassName("seconddiv");
+let hide = document.getElementsByClassName("hide");
+
 let mode = document.querySelector(".mode");
 let currentmode = "light";
 mode.addEventListener("click", () => {
   if (currentmode === "light") {
     currentmode = "dark";
-    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.backgroundColor = "rgb(42, 42, 42)";
+    document.querySelector("#mainheader").style.backgroundColor = "black";
+    mainheader.style.color = "white";
+    sidebar.style.backgroundColor = "black";
+    for (let i = 0; i < navoption.length; i++) {
+      navoption[i].style.color = "white";
+    }
+    for (let i = 0; i < aftermode.length; i++) {
+      aftermode[i].style.color = "white";
+    }
   } else {
     currentmode = "light";
     document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("#mainheader").style.backgroundColor =
+      "rgb(240, 239, 239)";
+    mainheader.style.color = "black";
+    sidebar.style.backgroundColor = "rgb(240, 239, 239)";
+    for (let i = 0; i < navoption.length; i++) {
+      navoption[i].style.color = "black";
+      // navoption[i].style.backgroundcolor = "rgb(152, 152, 152)";
+    }
+    for (let i = 0; i < aftermode.length; i++) {
+      aftermode[i].style.color = "black";
+    }
   }
 });
 
-// let mode = document.querySelector(".modebtn");
-// mode.innerText = "click to change mode";
-// let currentmode = "light";
-// mode.addEventListener("click", () => {
-//   if (currentmode === "light") {
-//     currentmode = "dark";
-//     document.querySelector("body").style.backgroundColor = "black";
-//     document.querySelector("#ananta").innerText = "ANANTA";
-//   } else {
-//     currentmode = "light";
-//     document.querySelector("body").style.backgroundColor = "white";
-//     document.querySelector("#rahim").innerText = "RAHIM";
-//   }
-//   console.log(currentmode);
-// });
+for (i = 0; i < seconddiv.length; i++) {
+  seconddiv[0].onmouseover = () => {
+    hide[0].classList.remove("hide");
+  };
+}
